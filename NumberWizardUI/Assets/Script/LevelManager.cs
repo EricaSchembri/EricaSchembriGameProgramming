29437d617bd68fc7e45e1c0c80e5072e9177c71f
Erica Schembri <b42460@mcast.org.mt>
2017-10-19 09:37:46 +0200

    This is the Third Project - Number Wizard UI

---
+++ NumberWizardUI/Assets/Script/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string levelName)
+    {
+        print("Loading Level " + levelName);
+
+        //loads the Scene named levelName
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Exit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
